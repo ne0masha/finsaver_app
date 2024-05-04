3442e33e67c96f8c689c3ed2e10d89eeb221c1de
Романов Макар Антонович <maanromanov@edu.hse.ru>
2024-05-04 17:31:04 +0300

    Created Repository class

---
+++ app/src/main/java/com/example/betaversionapp/data/repositories/AppRepository.kt
@@ -0,0 +1,19 @@
+package com.example.betaversionapp.data.repositories
+
+import com.example.betaversionapp.data.db.AppDatabase
+import com.example.betaversionapp.data.db.entities.Transaction
+
+class AppRepository(
+    private val db: AppDatabase
+) {
+    suspend fun upsert(transaction: Transaction) = db.getTransactionDao().upsert(transaction)
+
+    suspend fun delete(transaction: Transaction) = db.getTransactionDao().delete(transaction)
+
+    suspend fun getTransactionById(transactionId: Long) = db.getTransactionDao().getTransactionById(transactionId)
+
+    fun getTotalAmount() = db.getTransactionDao().getTotalAmount()
+
+    fun getAllTransactions() = db.getTransactionDao().getAllTransactions()
+
+}
